0ac9af6944366d87f49633d91c3315cf2e650057
leocavalcante <lc@leocavalcante.com>
2017-01-23 17:35:51 -0200

    Move twig ref to $GLOBALS avoiding side-effects on the file

--- src/Twig/Twig.php
+++ src/Twig/Twig.php
@@ -2,30 +2,28 @@
 
 namespace Siler\Twig;
 
-$_twigEnv = null;
-
 function init($templatesPath, $templatesCachePath = null, $debug = null)
 {
-    global $_twigEnv;
-
     if (is_null($debug)) {
         $debug = false;
     }
 
-    $_twigEnv = new \Twig_Environment(new \Twig_Loader_Filesystem($templatesPath), [
+    $twig = new \Twig_Environment(new \Twig_Loader_Filesystem($templatesPath), [
         'debug' => $debug,
         'cache' => $templatesCachePath,
     ]);
 
     if (function_exists('url')) {
-        $_twigEnv->addFunction(new \Twig_SimpleFunction('url', 'url'));
+        $twig->addFunction(new \Twig_SimpleFunction('url', 'url'));
     }
 
-    return $_twigEnv;
+    $GLOBALS['twig'] = $twig;
+
+    return $twig;
 }
 
 function render($name, $data = [])
 {
-    global $_twigEnv;
-    return $_twigEnv->render($name, $data);
+    $twig = $GLOBALS['twig'];
+    return $twig->render($name, $data);
 }
