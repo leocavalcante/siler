249f0a81019a57ea9029a0fec552699e9957feb1
leocavalcante <lc@leocavalcante.com>
2017-02-05 22:54:21 -0200

    Add route facades

--- src/Route/Route.php
+++ src/Route/Route.php
@@ -9,6 +9,61 @@ use Siler\Http;
 use function Siler\require_fn;
 
 /**
+ * Define a new route using the GET HTTP method
+ *
+ * @param string $path The HTTP URI to listen on
+ * @param string|callable $callback The callable to be executed or a string to be used with Siler\require_fn
+ */
+function get($path, $callback)
+{
+    route('get', $path, $callback);
+}
+
+/**
+ * Define a new route using the POST HTTP method
+ *
+ * @param string $path The HTTP URI to listen on
+ * @param string|callable $callback The callable to be executed or a string to be used with Siler\require_fn
+ */
+function post($path, $callback)
+{
+    route('post', $path, $callback);
+}
+
+/**
+ * Define a new route using the PUT HTTP method
+ *
+ * @param string $path The HTTP URI to listen on
+ * @param string|callable $callback The callable to be executed or a string to be used with Siler\require_fn
+ */
+function put($path, $callback)
+{
+    route('put', $path, $callback);
+}
+
+/**
+ * Define a new route using the DELETE HTTP method
+ *
+ * @param string $path The HTTP URI to listen on
+ * @param string|callable $callback The callable to be executed or a string to be used with Siler\require_fn
+ */
+function delete($path, $callback)
+{
+    route('delete', $path, $callback);
+}
+
+/**
+ * Define a new route using the OPTIONS HTTP method
+ *
+ * @param string $path The HTTP URI to listen on
+ * @param string|callable $callback The callable to be executed or a string to be used with Siler\require_fn
+ */
+function options($path, $callback)
+{
+    route('options', $path, $callback);
+}
+
+/**
  * Define a new route
  *
  * @param string $method The HTTP request method to listen on
---
+++ tests/Route/RouteFacadesTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Siler\Test;
+
+use PHPUnit\Framework\TestCase;
+use Siler\Route;
+
+class RouteFacadesTest extends TestCase
+{
+    protected function setUp()
+    {
+        $_GET = $_POST = $_REQUEST = ['foo' => 'bar'];
+
+        $_SERVER['HTTP_HOST'] = 'test:8000';
+        $_SERVER['SCRIPT_NAME'] = '/foo/test.php';
+        $_SERVER['REQUEST_URI'] = '/bar/baz';
+    }
+
+    /**
+     * @expectedException        \Exception
+     * @expectedExceptionMessage Route /bar/baz should match
+     */
+    public function testGet()
+    {
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+
+        Route\get('/bar/baz', function ($params) {
+            throw new \Exception('Route /bar/baz should match');
+        });
+    }
+
+    /**
+     * @expectedException        \Exception
+     * @expectedExceptionMessage Route /bar/baz should match
+     */
+    public function testPost()
+    {
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+
+        Route\post('/bar/baz', function ($params) {
+            throw new \Exception('Route /bar/baz should match');
+        });
+    }
+
+    /**
+     * @expectedException        \Exception
+     * @expectedExceptionMessage Route /bar/baz should match
+     */
+    public function testPut()
+    {
+        $_SERVER['REQUEST_METHOD'] = 'PUT';
+
+        Route\put('/bar/baz', function ($params) {
+            throw new \Exception('Route /bar/baz should match');
+        });
+    }
+
+    /**
+     * @expectedException        \Exception
+     * @expectedExceptionMessage Route /bar/baz should match
+     */
+    public function testDelete()
+    {
+        $_SERVER['REQUEST_METHOD'] = 'DELETE';
+
+        Route\delete('/bar/baz', function ($params) {
+            throw new \Exception('Route /bar/baz should match');
+        });
+    }
+
+    /**
+     * @expectedException        \Exception
+     * @expectedExceptionMessage Route /bar/baz should match
+     */
+    public function testOptions()
+    {
+        $_SERVER['REQUEST_METHOD'] = 'OPTIONS';
+
+        Route\options('/bar/baz', function ($params) {
+            throw new \Exception('Route /bar/baz should match');
+        });
+    }
+}
