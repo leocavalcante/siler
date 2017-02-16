67082ab8809192910ed2ed3d82f34f8930542d86
leocavalcante <lc@leocavalcante.com>
2017-02-16 18:29:35 -0200

    WIP Ws tests

--- src/Ws/Ws.php
+++ src/Ws/Ws.php
@@ -8,7 +8,6 @@ use function Siler\array_get;
 use Ratchet\WebSocket\WsServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\Server\IoServer;
-use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
 function init($port = null)
@@ -61,46 +60,5 @@ function broadcast($message, ConnectionInterface $from = null)
         }
 
         $client->send($message);
-    }
-}
-
-class MessageComponent implements MessageComponentInterface
-{
-    public function onOpen(ConnectionInterface $conn)
-    {
-        Container\get('ws_clients')->attach($conn);
-        $this->callback('open', [$conn]);
-    }
-
-    public function onMessage(ConnectionInterface $from, $message)
-    {
-        $this->callback('message', [$from, $message]);
-    }
-
-    public function onClose(ConnectionInterface $conn)
-    {
-        Container\get('ws_clients')->detach($conn);
-        $this->callback('close', [$conn]);
-    }
-
-    public function onError(ConnectionInterface $conn, \Exception $e)
-    {
-        $this->callback('error', [$conn, $e]);
-        $conn->close();
-    }
-
-    private function callback($name, $params)
-    {
-        $callback = Container\get('ws_on_'.$name);
-
-        if (is_null($callback)) {
-            return;
-        }
-
-        if (!is_callable($callback)) {
-            return;
-        }
-
-        call_user_func_array($callback, $params);
     }
 }
