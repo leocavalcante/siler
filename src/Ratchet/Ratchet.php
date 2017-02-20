ae17c8305c518800686b691c5a3d2e7c9a4ead1e
leocavalcante <lc@leocavalcante.com>
2017-02-20 17:15:17 -0300

    Add DocBlocks

--- src/Ratchet/Ratchet.php
+++ src/Ratchet/Ratchet.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * Helper functions to work with Ratchet
+ */
 
 namespace Siler\Ratchet;
 
@@ -15,6 +18,11 @@ const RATCHET_EVENT_MESSAGE = 'ratchet_event_message';
 const RATCHET_EVENT_CLOSE = 'ratchet_event_close';
 const RATCHET_EVENT_ERROR = 'ratchet_event_error';
 
+/**
+ * Initialize the Ratchet server. Note: this blocks the reset of code execution
+ *
+ * @param int $port The port number on which the server should run. Defaults to 8080
+ */
 function init($port = null)
 {
     if (is_null($port)) {
@@ -30,26 +38,52 @@ function init($port = null)
     $server->run();
 }
 
+/**
+ * Sets a callback for connected clients
+ *
+ * @param \Closure $callback The callback function to call when there is a new client connected
+ */
 function connected($callback)
 {
     Container\set(RATCHET_EVENT_OPEN, $callback);
 }
 
+/**
+ * Sets a callback for incoming websocket messages
+ *
+ * @param  \Closure $callback The callback for incoming messages
+ */
 function inbox($callback)
 {
     Container\set(RATCHET_EVENT_MESSAGE, $callback);
 }
 
+/**
+ * Sets a callback for closed connections
+ *
+ * @param  \Closure $callback The callback for closed connections
+ */
 function closed($callback)
 {
     Container\set(RATCHET_EVENT_CLOSE, $callback);
 }
 
+/**
+ * Sets a callback to handle errors
+ *
+ * @param  \Closure $callback The callback for errors
+ */
 function error($callback)
 {
     Container\set(RATCHET_EVENT_ERROR, $callback);
 }
 
+/**
+ * Broadcast a message for the connected clients
+ *
+ * @param  string $message The message to broadcast
+ * @param  ConnectionInterface|null $from The client who is sending the message, if given the message will not be broadcasted to it
+ */
 function broadcast($message, ConnectionInterface $from = null)
 {
     $clients = Container\get(RATCHET_CONNECTIONS);
