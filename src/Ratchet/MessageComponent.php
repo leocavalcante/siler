15e0a3105ea76a3f2fd07a6ea14c55173e82c4d1
leocavalcante <lc@leocavalcante.com>
2017-02-20 17:19:20 -0300

    Add DocBlocks

--- src/Ratchet/MessageComponent.php
+++ src/Ratchet/MessageComponent.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * Siler's internal MessageComponent
+ */
 
 namespace Siler\Ratchet;
 
@@ -6,32 +9,53 @@ use Siler\Container;
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
+/**
+ * @internal Not meant to be used
+ */
 class MessageComponent implements MessageComponentInterface
 {
+    /**
+     * {@inherit}
+     */
     public function onOpen(ConnectionInterface $conn)
     {
         Container\get(RATCHET_CONNECTIONS)->attach($conn);
         $this->callback(RATCHET_EVENT_OPEN, [$conn]);
     }
 
+    /**
+     * {@inherit}
+     */
     public function onMessage(ConnectionInterface $from, $message)
     {
         $this->callback(RATCHET_EVENT_MESSAGE, [$from, $message]);
     }
 
+    /**
+     * {@inherit}
+     */
     public function onClose(ConnectionInterface $conn)
     {
         Container\get(RATCHET_CONNECTIONS)->detach($conn);
         $this->callback(RATCHET_EVENT_CLOSE, [$conn]);
     }
 
+    /**
+     * {@inherit}
+     */
     public function onError(ConnectionInterface $conn, \Exception $exception)
     {
         $this->callback(RATCHET_EVENT_ERROR, [$conn, $exception]);
         $conn->close();
     }
 
-    private function callback($ratchetEvent, $params)
+    /**
+     * Helper function to call event callbacks checking for its existence
+     *
+     * @param  string $ratchetEvent The event name used in the Siler\Container
+     * @param  array $params The array of params to be used as arguments
+     */
+    private function callback($ratchetEvent, array $params)
     {
         $callback = Container\get($ratchetEvent);
 
