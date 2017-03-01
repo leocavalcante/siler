858220c5757ab96cf9bace155fc4ef4490f01dbc
leocavalcante <lc@leocavalcante.com>
2017-03-01 18:18:16 -0300

    It seams that we can have a agreement using λ as namespace alias

--- tests/Functional/FunctionalTest.php
+++ tests/Functional/FunctionalTest.php
@@ -2,53 +2,55 @@
 
 namespace Siler\Test;
 
+use Siler\Functional as f;
+
 class FunctionalTest extends \PHPUnit\Framework\TestCase
 {
     public function testId()
     {
-        $this->assertSame('foo', λ\identity()('foo'));
+        $this->assertSame('foo', f\identity()('foo'));
     }
 
     public function testAlways()
     {
-        $this->assertSame('foo', λ\always('foo')());
-        $this->assertSame('foo', λ\always('foo')('bar'));
+        $this->assertSame('foo', f\always('foo')());
+        $this->assertSame('foo', f\always('foo')('bar'));
     }
 
     public function testEq()
     {
-        $this->assertTrue(λ\equal('foo')('foo'));
-        $this->assertTrue(λ\equal(1)(1));
-        $this->assertFalse(λ\equal(1)('1'));
-        $this->assertFalse(λ\equal(1)(true));
+        $this->assertTrue(f\equal('foo')('foo'));
+        $this->assertTrue(f\equal(1)(1));
+        $this->assertFalse(f\equal(1)('1'));
+        $this->assertFalse(f\equal(1)(true));
     }
 
     public function testLt()
     {
-        $this->assertTrue(λ\less_than(2)(1));
-        $this->assertFalse(λ\less_than(2)(2));
-        $this->assertFalse(λ\less_than(2)(3));
+        $this->assertTrue(f\less_than(2)(1));
+        $this->assertFalse(f\less_than(2)(2));
+        $this->assertFalse(f\less_than(2)(3));
     }
 
     public function testGt()
     {
-        $this->assertFalse(λ\greater_than(2)(1));
-        $this->assertFalse(λ\greater_than(2)(2));
-        $this->assertTrue(λ\greater_than(2)(3));
+        $this->assertFalse(f\greater_than(2)(1));
+        $this->assertFalse(f\greater_than(2)(2));
+        $this->assertTrue(f\greater_than(2)(3));
     }
 
     public function testIfe()
     {
-        $foo = λ\if_else(λ\identity())(λ\always('foo'))(λ\always('bar'));
+        $foo = f\if_else(f\identity())(f\always('foo'))(f\always('bar'));
         $this->assertSame('foo', $foo(true));
     }
 
     public function testMatch()
     {
-        $test = λ\match([
-            [λ\equal('foo'), λ\always('bar')],
-            [λ\equal('bar'), λ\always('baz')],
-            [λ\equal('baz'), λ\always('qux')],
+        $test = f\match([
+            [f\equal('foo'), f\always('bar')],
+            [f\equal('bar'), f\always('baz')],
+            [f\equal('baz'), f\always('qux')],
         ]);
 
         $this->assertSame('bar', $test('foo'));
@@ -59,7 +61,7 @@ class FunctionalTest extends \PHPUnit\Framework\TestCase
 
     public function testAny()
     {
-        $test = λ\any([λ\equal(2), λ\greater_than(2)]);
+        $test = f\any([f\equal(2), f\greater_than(2)]);
 
         $this->assertFalse($test(1));
         $this->assertTrue($test(2));
@@ -68,32 +70,32 @@ class FunctionalTest extends \PHPUnit\Framework\TestCase
 
     public function testAll()
     {
-        $this->assertTrue(λ\all([λ\less_than(2), λ\less_than(3)])(1));
-        $this->assertFalse(λ\all([λ\equal(1), λ\greater_than(1)])(1));
+        $this->assertTrue(f\all([f\less_than(2), f\less_than(3)])(1));
+        $this->assertFalse(f\all([f\equal(1), f\greater_than(1)])(1));
     }
 
     public function testNot()
     {
-        $this->assertTrue(λ\not(λ\equal(2))(3));
-        $this->assertFalse(λ\not(λ\equal(2))(2));
+        $this->assertTrue(f\not(f\equal(2))(3));
+        $this->assertFalse(f\not(f\equal(2))(2));
     }
 
     public function testMath()
     {
-        $this->assertSame(2, λ\add(1)(1));
-        $this->assertSame(1, λ\sub(2)(3));
-        $this->assertSame(4, λ\mul(2)(2));
-        $this->assertSame(2, λ\div(2)(4));
-        $this->assertSame(-1, λ\sub(3)(2));
-        $this->assertSame(.5, λ\div(4)(2));
+        $this->assertSame(2, f\add(1)(1));
+        $this->assertSame(1, f\sub(2)(3));
+        $this->assertSame(4, f\mul(2)(2));
+        $this->assertSame(2, f\div(2)(4));
+        $this->assertSame(-1, f\sub(3)(2));
+        $this->assertSame(.5, f\div(4)(2));
     }
 
     public function testCompose()
     {
-        $test = λ\compose([λ\add(2), λ\mul(2)]);
+        $test = f\compose([f\add(2), f\mul(2)]);
         $this->assertSame(8, $test(2));
 
-        $test = λ\compose([λ\div(2), λ\sub(1)]);
+        $test = f\compose([f\div(2), f\sub(1)]);
         $this->assertSame(0, $test(2));
     }
 }
