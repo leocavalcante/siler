d5415c2595d8b9e9f3e7258aa8a7ef8caf4964cb
leocavalcante <lc@leocavalcante.com>
2017-04-20 13:50:14 -0300

    Refactor example. API working. Add queries examples

--- examples/graphql-subscriptions/resolvers.php
+++ examples/graphql-subscriptions/resolvers.php
@@ -6,26 +6,54 @@ use Siler\Graphql;
 R::setup('sqlite:'.__DIR__.'/db.sqlite');
 Graphql\subscriptions_at('ws://127.0.0.1:8080');
 
+$roomByName = function ($name) {
+    return R::findOne('room', 'name = ?', [$name]);
+};
+
+$messages = function ($root, $args) use ($roomByName) {
+    $roomName = $args['roomName'];
+    $room = $roomByName($roomName);
+    $messages = R::find('message', 'room_id = ?', [$room['id']]);
+
+    return $messages;
+};
+
+$rooms = function () {
+    return R::findAll('room');
+};
+
+$start = function ($root, $args) {
+    $roomName = $args['roomName'];
+
+    $room = R::dispense('room');
+    $room['name'] = $roomName;
+
+    R::store($room);
+
+    return $room;
+};
+
+$chat = function ($root, $args) use ($roomByName) {
+    $roomName = $args['roomName'];
+    $body = $args['body'];
+
+    $room = $roomByName($roomName);
+
+    $message = R::dispense('message');
+    $message['roomId'] = $room['id'];
+    $message['body'] = $body;
+    $message['timestamp'] = new \DateTime();
+
+    R::store($message);
+
+    return $message;
+};
+
 return [
-    'Query' => [
-        'messages' => function ($root, $args) {
-            return R::findAll('message');
-        },
-    ],
-    'Mutation' => [
-        'addMessage' => function ($root, $args) {
-            $message = R::dispense('message');
-            $message->body = $args['body'];
-
-            R::store($message);
-
-            Graphql\publish('newMessage', $message);
-
-            return $message;
-        },
-    ],
+    'Query' => compact('rooms', 'messages'),
+    'Mutation' => compact('start', 'chat'),
     'Subscription' => [
-        'newMessage' => function ($root, $args) {
+        'inbox' => function ($root, $args) {
             return $root;
         },
     ],
--- examples/graphql-subscriptions/schema.php
+++ examples/graphql-subscriptions/schema.php
@@ -5,6 +5,4 @@ use Siler\Graphql;
 $typeDefs = file_get_contents(__DIR__.'/schema.graphql');
 $resolvers = include __DIR__.'/resolvers.php';
 
-Graphql\resolvers($resolvers);
-
-return Graphql\schema($typeDefs);
+return Graphql\schema($typeDefs, $resolvers);
