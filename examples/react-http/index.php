7fe8e4c35681cf422679e2302849c240b36b300d
leocavalcante <lc@leocavalcante.com>
2018-01-25 23:24:41 -0200

    Update react/http example

--- examples/react-http/index.php
+++ examples/react-http/index.php
@@ -1,9 +1,5 @@
 <?php
 
-use Psr\Http\Message\RequestInterface;
-use React\EventLoop\Factory;
-use React\Promise\Promise;
-use React\Socket\Server;
 use Siler\Diactoros;
 use Siler\Functional as F;
 use Siler\Route;
@@ -14,25 +10,22 @@ require 'vendor/autoload.php';
 
 Twig\init('examples/react-http');
 
-$loop = Factory::create();
-$socket = new Server(isset($argv[1]) ? $argv[1] : '0.0.0.0:0', $loop);
-$server = new \React\Http\Server($socket, function (RequestInterface $request) {
-    return new Promise(function ($resolve, $reject) use ($request) {
-        try {
-            Route\psr7($request);
+$loop = React\EventLoop\Factory::create();
 
-            $response = Diactoros\text('not found', 404);
-            $response = Route\get('/baz', 'examples/react-http/baz.php') ?: $response;
-            $response = Route\get('/foo', F\always(Diactoros\text('foo'))) ?: $response;
-            $response = Route\get('/', F\always(Diactoros\text('home'))) ?: $response;
+$server = new React\Http\Server(function (Psr\Http\Message\ServerRequestInterface $request) {
+    Route\psr7($request);
 
-            $resolve($response);
-        } catch (Exception $e) {
-            $resolve(Diactoros\text($e->getMessage(), 500));
-        }
-    });
+    $response = Diactoros\text('not found', 404);
+    $response = Route\get('/baz', 'examples/react-http/baz.php') ?? $response;
+    $response = Route\get('/foo', F\always(Diactoros\text('foo'))) ?? $response;
+    $response = Route\get('/', F\always(Diactoros\text('home'))) ?? $response;
+
+    return $response;
 });
 
-echo 'Listening on http://'.$socket->getAddress().PHP_EOL;
+$socket = new React\Socket\Server(8080, $loop);
+$server->listen($socket);
+
+echo "Server running at http://127.0.0.1:8080\n";
 
 $loop->run();
