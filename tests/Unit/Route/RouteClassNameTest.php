7edb184006966e78868481d8856430873d4fa9d3
leocavalcante <lc@leocavalcante.com>
2018-10-20 18:08:08 -0300

    Add class_name Route feature, works like Controllers on other frameworks, closes #109

---
+++ tests/Unit/Route/RouteClassNameTest.php
@@ -0,0 +1,84 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Siler\Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Siler\Route;
+
+class MyClass
+{
+    public function getIndex()
+    {
+        echo 'className.index';
+    }
+
+    public function postFoo()
+    {
+        echo 'className.postFoo';
+    }
+
+    public function putFooBar()
+    {
+        echo 'className.putFooBar';
+        Route\stop_propagation();
+    }
+
+    public function anyIndex(string $baz, string $qux)
+    {
+        echo "className.$baz.$qux";
+        Route\stop_propagation();
+    }
+
+}
+
+class RouteClassNameTest extends TestCase
+{
+    public function testIndex()
+    {
+        $this->expectOutputString('className.index');
+
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_SERVER['REQUEST_URI'] = '/class-name';
+
+        Route\class_name('/class-name', 'Siler\Test\Unit\MyClass');
+    }
+
+    public function testPostFoo()
+    {
+        $this->expectOutputString('className.postFoo');
+
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        $_SERVER['REQUEST_URI'] = '/class-name/foo';
+
+        Route\class_name('/class-name', 'Siler\Test\Unit\MyClass');
+    }
+
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testPutFooBar()
+    {
+        $this->expectOutputString('className.putFooBar');
+
+        $_SERVER['REQUEST_METHOD'] = 'PUT';
+        $_SERVER['REQUEST_URI'] = '/class-name/foo/bar';
+
+        Route\class_name('/class-name', 'Siler\Test\Unit\MyClass');
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testAnyParams()
+    {
+        $this->expectOutputString('className.baz.qux');
+
+        $_SERVER['REQUEST_METHOD'] = 'ANYTHING';
+        $_SERVER['REQUEST_URI'] = '/class-name/baz/qux';
+
+        Route\class_name('/class-name', 'Siler\Test\Unit\MyClass');
+    }
+}
