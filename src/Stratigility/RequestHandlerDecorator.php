0807881eef74de5451b1acb44433cb8844c92e5f
leocavalcante <lc@leocavalcante.com>
2018-12-07 15:10:02 -0200

    Using Middlewares within Siler routes

---
+++ src/Stratigility/RequestHandlerDecorator.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * Siler's internal MessageComponent.
+ */
+
+namespace Siler\Stratigility;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * @internal Not meant to be used
+ */
+class RequestHandlerDecorator implements RequestHandlerInterface
+{
+    private $handler;
+    private $pathParams;
+
+    public function __construct(callable $handler, array $pathParams = [])
+    {
+        $this->handler = $handler;
+        $this->pathParams = $pathParams;
+    }
+
+    /**
+     * {@inheritdoc}.
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $handler = $this->handler;
+        return $handler($request, $this->pathParams);
+    }
+}
