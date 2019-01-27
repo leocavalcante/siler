d4febeb7a92c86d57cf6749b1d570887c7272cb1
leocavalcante <lc@leocavalcante.com>
2019-01-27 15:32:27 -0200

    Add Monolog Support

---
+++ examples/monolog/index.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+require_once __DIR__.'/../../vendor/autoload.php';
+
+use Siler\Monolog as Log;
+
+Log\handler(Log\stream(__DIR__.'/siler.log'));
+
+Log\debug('debug', ['level' => 'debug']);
+Log\info('info', ['level' => 'info']);
+Log\notice('notice', ['level' => 'notice']);
+Log\warning('warning', ['level' => 'warning']);
+Log\error('error', ['level' => 'error']);
+Log\critical('critical', ['level' => 'critical']);
+Log\alert('alert', ['level' => 'alert']);
+Log\emergency('emergency', ['level' => 'emergency']);
