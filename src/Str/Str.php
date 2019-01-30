f463cf0b935633a245661411e070548a33a67ee4
leocavalcante <lc@leocavalcante.com>
2019-01-30 21:46:49 -0200

    Add DocBlocks

--- src/Str/Str.php
+++ src/Str/Str.php
@@ -1,12 +1,22 @@
 <?php
 
 declare(strict_types=1);
-
+/**
+ * String module. Functions to operate on strings.
+ */
 namespace Siler\Str;
 
 use Cocur\Slugify\Slugify;
 use Siler\Container;
 
+/**
+ * Slugify a string
+ *
+ * @param string $input
+ * @param array|null $opts
+ *
+ * @return string
+ */
 function slugify(string $input, ?array $opts = null): string
 {
     if (!Container\has('slugify')) {
