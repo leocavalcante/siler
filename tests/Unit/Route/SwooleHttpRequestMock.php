0921416ce2690e9cee985ecbce66574523250711
leocavalcante <lc@leocavalcante.com>
2019-02-03 13:46:08 -0200

    Improved Route Testing

---
+++ tests/Unit/Route/SwooleHttpRequestMock.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siler\Test\Unit\Route;
+
+class SwooleHttpRequestMock
+{
+    public $server;
+
+    public function __construct(string $method, string $uri)
+    {
+        $this->server = [
+            'request_method' => $method,
+            'request_uri' => $uri,
+        ];
+    }
+}
