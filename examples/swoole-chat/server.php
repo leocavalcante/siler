c4a826f1c02a29f180f5aad630d3e86f46ca9acd
leocavalcante <lc@leocavalcante.com>
2019-02-02 22:47:15 -0200

    Add Swoole WebSocket

---
+++ examples/swoole-chat/server.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+require_once __DIR__.'/../../vendor/autoload.php';
+
+use Siler\Swoole;
+
+$echo = function ($frame) {
+    Swoole\broadcast($frame->data);
+};
+
+$onOpen = function () {
+    echo "Connection stabilised\n";
+};
+
+$onClose = function () {
+    echo "Connection closed\n";
+};
+
+Swoole\websocket(9502)($echo, $onOpen, $onClose);
