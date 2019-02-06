b4d31ef6200b2bc7331f0919cba2115b948ba42e
leocavalcante <lc@leocavalcante.com>
2019-02-05 23:39:48 -0200

    Default MongoDB database name and basic CRUD APIs

--- src/Db/Mongo.php
+++ src/Db/Mongo.php
@@ -10,6 +10,7 @@ namespace Siler\Mongo;
 use Siler\Container;
 
 const MONGODB_DEFAULT_NAME = 'mongodb';
+const MONGODB_USING_DBNAME = 'mongodb_dbname';
 
 /**
  * Creates a new MongoDB\Client instance.
@@ -76,4 +77,60 @@ function collection(string $databaseName, string $collectionName, array $options
 function oid(string $oid): \MongoDB\BSON\ObjectId
 {
     return new \MongoDB\BSON\ObjectId($oid);
+}
+
+function using(string $databaseName)
+{
+    Container\set(MONGODB_USING_DBNAME, $databaseName);
+}
+
+function find(string $collectionName, array $filter = [], array $options = []): \MongoDB\Driver\Cursor
+{
+    return collection(__get_dbname_or_throw(), $collectionName)->find($filter, $options);
+}
+
+function find_one(string $collectionName, array $filter = [], array $options = [])
+{
+    return collection(__get_dbname_or_throw(), $collectionName)->findOne($filter, $options);
+}
+
+function insert_many(string $collectionName, array $documents, array $options = []): \MongoDB\InsertManyResult
+{
+    return collection(__get_dbname_or_throw(), $collectionName)->insertMany($documents, $options);
+}
+
+function insert_one(string $collectionName, $document, array $options = []): \MongoDB\InsertOneResult
+{
+    return collection(__get_dbname_or_throw(), $collectionName)->insertOne($document, $options);
+}
+
+function update_one(string $collectionName, array $filter, $update, array $options = []): \MongoDB\UpdateResult
+{
+    return collection(__get_dbname_or_throw(), $collectionName)->updateOne($filter, $update, $options);
+}
+
+function update_many(string $collectionName, array $filter, $update, array $options = []): \MongoDB\UpdateResult
+{
+    return collection(__get_dbname_or_throw(), $collectionName)->updateMany($filter, $update, $filter);
+}
+
+function delete_one(string $collectionName, array $filter, array $options = []): \MongoDB\DeleteResult
+{
+    return colletion(__get_dbname_or_throw(), $collectionName)->deleteOne($filter, $options);
+}
+
+function delete_many(string $collectionName, array $filter, array $options = []): \MongoDB\DeleteResult
+{
+    return collection(__get_dbname_or_throw(), $collectionName)->deleteMany($filter, $options);
+}
+
+function __get_dbname_or_throw(): string
+{
+    $databaseName = Container\get(MONGODB_USING_DBNAME);
+
+    if (is_null($databaseName)) {
+        throw new \UnderflowException('No Mongo Database name set');
+    }
+
+    return $databaseName;
 }
