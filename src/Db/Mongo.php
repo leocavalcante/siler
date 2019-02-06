1634631bdfbc8b0b054520052e9fc8c04e832581
leocavalcante <lc@leocavalcante.com>
2019-02-06 19:43:22 -0200

    Add DocBlocks

--- src/Db/Mongo.php
+++ src/Db/Mongo.php
@@ -79,51 +79,135 @@ function oid(string $oid): \MongoDB\BSON\ObjectId
     return new \MongoDB\BSON\ObjectId($oid);
 }
 
+/**
+ * Sets a default database name.
+ *
+ * @param string $databaseName
+ */
 function using(string $databaseName)
 {
     Container\set(MONGODB_USING_DBNAME, $databaseName);
 }
 
+/**
+ * Find operation on the default database and in the given collection.
+ *
+ * @param string $collectionName
+ * @param array $filter
+ * @param array $options
+ *
+ * @return \MongoDB\Driver\Cursor
+ */
 function find(string $collectionName, array $filter = [], array $options = []): \MongoDB\Driver\Cursor
 {
     return collection(__get_dbname_or_throw(), $collectionName)->find($filter, $options);
 }
 
+/**
+ * Find one operation on the default database and in the given collection.
+ *
+ * @param string $collectionName
+ * @param array $filter
+ * @param array $options
+ *
+ * @return array|object|null
+ */
 function find_one(string $collectionName, array $filter = [], array $options = [])
 {
     return collection(__get_dbname_or_throw(), $collectionName)->findOne($filter, $options);
 }
 
+/**
+ * Insert many operation on the default database and in the given collection.
+ *
+ * @param string $collectionName
+ * @param array $documents
+ * @param array $options
+ *
+ * @return \MongoDB\InsertManyResult
+ */
 function insert_many(string $collectionName, array $documents, array $options = []): \MongoDB\InsertManyResult
 {
     return collection(__get_dbname_or_throw(), $collectionName)->insertMany($documents, $options);
 }
 
+/**
+ * Insert one operation on the default database and in the given collection.
+ *
+ * @param string $collectionName
+ * @param $document
+ * @param array $options
+ *
+ * @return \MongoDB\InsertOneResult
+ */
 function insert_one(string $collectionName, $document, array $options = []): \MongoDB\InsertOneResult
 {
     return collection(__get_dbname_or_throw(), $collectionName)->insertOne($document, $options);
 }
 
+/**
+ * Update one operation on the default database and in the given colleciton.
+ *
+ * @param string $collectionName
+ * @param array $filter
+ * @param $update
+ * @param array $options
+ *
+ * @return \MongoDB\UpdateResult
+ */
 function update_one(string $collectionName, array $filter, $update, array $options = []): \MongoDB\UpdateResult
 {
     return collection(__get_dbname_or_throw(), $collectionName)->updateOne($filter, $update, $options);
 }
 
+/**
+ * Update many operation on the default database and in the given collection.
+ *
+ * @param string $collectionName
+ * @param array $filter
+ * @param $update
+ * @param array $options
+ *
+ * @return \MongoDB\UpdateResult
+ */
 function update_many(string $collectionName, array $filter, $update, array $options = []): \MongoDB\UpdateResult
 {
     return collection(__get_dbname_or_throw(), $collectionName)->updateMany($filter, $update, $filter);
 }
 
+/**
+ * Delete one operation on the default database and on the given collection.
+ *
+ * @param string $collectionName
+ * @param array $filter
+ * @param array $options
+ *
+ * @return \MongoDB\DeleteResult
+ */
 function delete_one(string $collectionName, array $filter, array $options = []): \MongoDB\DeleteResult
 {
-    return colletion(__get_dbname_or_throw(), $collectionName)->deleteOne($filter, $options);
+    return collection(__get_dbname_or_throw(), $collectionName)->deleteOne($filter, $options);
 }
 
+/**
+ * Delete many operation on the default database and on the given collection.
+ *
+ * @param string $collectionName
+ * @param array $filter
+ * @param array $options
+ *
+ * @return \MongoDB\DeleteResult
+ */
 function delete_many(string $collectionName, array $filter, array $options = []): \MongoDB\DeleteResult
 {
     return collection(__get_dbname_or_throw(), $collectionName)->deleteMany($filter, $options);
 }
 
+/**
+ * @internal Gets the default database name or throws an Exception.
+ *
+ * @return string
+ */
 function __get_dbname_or_throw(): string
 {
     $databaseName = Container\get(MONGODB_USING_DBNAME);
