f64a01d81cbf60e8e2d20d6815004634c45fc2ad
leocavalcante <lc@leocavalcante.com>
2019-02-12 20:43:50 -0200

    Add some tests to Mail module and MongoDB module

--- tests/Unit/Db/MongoTest.php
+++ tests/Unit/Db/MongoTest.php
@@ -6,6 +6,7 @@ namespace Siler\Test\Unit;
 
 use PHPUnit\Framework\TestCase;
 use Siler\Mongo;
+use Siler\Container;
 
 class MongoTest extends TestCase
 {
@@ -26,5 +27,17 @@ class MongoTest extends TestCase
     public function testCollectionOutOfRange()
     {
         Mongo\collection('', '');
+    }
+
+    public function testOid()
+    {
+        $oid = Mongo\oid('5a2493c33c95a1281836eb6a');
+        $this->assertInstanceOf(\MongoDB\BSON\ObjectId::class, $oid);
+    }
+
+    public function testUsing()
+    {
+        Mongo\using('test');
+        $this->assertSame('test', Container\get(Mongo\MONGODB_USING_DBNAME));
     }
 }
