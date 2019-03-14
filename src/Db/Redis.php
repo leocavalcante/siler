0e0a676ceaaa586aad61f1a184d3b15d53494d92
leocavalcante <lc@leocavalcante.com>
2019-03-14 14:20:02 -0300

    redis: draft

---
+++ src/Db/Redis.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace  Siler\Redis;
+
+use Siler\Container;
+
+const DEFAULT_INSTANCE = 'redis_default_instance';
+
+function connect(string $host = '127.0.0.1', int $port = 6379, string $redisInstance = DEFAULT_INSTANCE): Redis
+{
+    $redis = new Redis();
+    $redis->connect($host, $port);
+
+    Container\set($redisInstance, $redis);
+
+    return $redis;
+}
+
+
+function get(string $key, string $redisInstance = DEFAULT_INSTANCE)
+{
+    $redis = Container\get(DEFAULT_INSTANCE);
+    return $redis->get($key);
+}
+
+function set(string $key, string $val, string $redisInstance = DEFAULT_INSTANCE)
+{
+    $redis = Container\get(DEFAULT_INSTANCE);
+    return $redis->set($key, $val);
+}
