99ea9259265858f7a04e243df438cea38038dfdd
leocavalcante <lc@leocavalcante.com>
2019-03-14 15:07:30 -0300

    redis: fix instance usage and add docblocks

--- src/Db/Redis.php
+++ src/Db/Redis.php
@@ -4,8 +4,20 @@ namespace  Siler\Redis;
 
 use Siler\Container;
 
+/**
+ * Default Redis instance name on Siler\Container.
+ */
 const DEFAULT_INSTANCE = 'redis_default_instance';
 
+/**
+ * Creates an instance and connects to a Redis server.
+ *
+ * @param string $host
+ * @param int $port
+ * @param string $redisInstance
+ *
+ * @return \Redis
+ */
 function connect(string $host = '127.0.0.1', int $port = 6379, string $redisInstance = DEFAULT_INSTANCE): \Redis
 {
     $redis = new \Redis();
@@ -16,21 +28,44 @@ function connect(string $host = '127.0.0.1', int $port = 6379, string $redisInst
     return $redis;
 }
 
-
+/**
+ * Gets the value from the given $key.
+ *
+ * @param string $key
+ * @param string $redisInstance
+ *
+ * @return mixed
+ */
 function get(string $key, string $redisInstance = DEFAULT_INSTANCE)
 {
-    $redis = Container\get(DEFAULT_INSTANCE);
+    $redis = Container\get($redisInstance);
     return $redis->get($key);
 }
 
+/**
+ * Sets a value on the given $key.
+ *
+ * @param string $key
+ * @param string $val
+ * @param string $redisInstance
+ *
+ * @return mixed
+ */
 function set(string $key, string $val, string $redisInstance = DEFAULT_INSTANCE)
 {
-    $redis = Container\get(DEFAULT_INSTANCE);
+    $redis = Container\get($redisInstance);
     return $redis->set($key, $val);
 }
 
-function has(string $key): bool
+/**
+ * Checks if the key exists.
+ *
+ * @param string $key
+ *
+ * @return bool
+ */
+function has(string $key, string $redisInstance = DEFAULT_INSTANCE): bool
 {
-    $redis = Container\get(DEFAULT_INSTANCE);
+    $redis = Container\get($redisInstance);
     return $redis->exists($key) > 0;
 }
