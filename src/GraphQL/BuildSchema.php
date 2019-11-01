8efe8938649fd39d45c9f907714c52d2731c727d
leocavalcante <lc@leocavalcante.com>
2019-11-01 16:28:23 -0300

    hotfix(graphql): checking for type in the resolvers array

--- src/GraphQL/BuildSchema.php
+++ src/GraphQL/BuildSchema.php
@@ -85,7 +85,7 @@ class BuildSchema
                     if (!empty($this->nodeMap[$typeName])) {
                         throw new Error(sprintf('Type "%s" was defined more than once.', $typeName));
                     }
-                    if (in_array($typeName, $this->resolvers) && $d->kind === NodeKind::SCALAR_TYPE_DEFINITION) {
+                    if (array_key_exists($typeName, $this->resolvers) && $d->kind === NodeKind::SCALAR_TYPE_DEFINITION) {
                         break;
                     }
                     $typeDefs[] = $d;
