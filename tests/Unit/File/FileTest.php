1a0a21954d5945689db35e5228a8822bf74b4cff
leocavalcante <lc@leocavalcante.com>
2019-11-25 23:17:40 -0300

    chore

--- tests/Unit/File/FileTest.php
+++ tests/Unit/File/FileTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Siler\Test\Unit\File;
 
 use PHPUnit\Framework\TestCase;
+
 use function Siler\File\concat_files;
 use function Siler\File\recur_iter_dir;
 use function Siler\File\recursively_iterated_directory;
