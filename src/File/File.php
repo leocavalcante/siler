37ca8f101a27693f6abe9280c2eb0ed243cc804c
leocavalcante <lc@leocavalcante.com>
2019-11-25 21:45:11 -0300

    Add concat files

--- src/File/File.php
+++ src/File/File.php
@@ -2,6 +2,8 @@
 
 namespace Siler\File;
 
+use function Siler\Functional\concat;
+
 /**
  * Returns a list of FileInfo objects recursively found in the given directory.
  *
@@ -18,4 +20,31 @@ function recursively_iterated_directory(string $dirname, string $regex = '/.*/',
     $regexp_iterator = new \RegexIterator($iterator, $regex, $mode);
 
     return array_values(iterator_to_array($regexp_iterator));
+}
+
+/**
+ * Alias for recursively_iterated_directory().
+ *
+ * @param string $dirname
+ * @param string $regex
+ * @param int $mode
+ *
+ * @return \SplFileInfo[]
+ */
+function recur_iter_dir(string $dirname, string $regex = '/.*/', $mode = \RegexIterator::MATCH): array
+{
+    return recursively_iterated_directory($dirname, $regex, $mode);
+}
+
+/**
+ * Loads and concatenates file contents.
+ *
+ * @param string[]|\SplFileInfo[] $files
+ *
+ * @return string
+ */
+function concat_files(array $files): string
+{
+    $files = array_map('file_get_contents', $files);
+    return trim(array_reduce($files, concat("\n"), ''));
 }
--- tests/Unit/File/FileTest.php
+++ tests/Unit/File/FileTest.php
@@ -3,6 +3,8 @@
 namespace Siler\Test\Unit\File;
 
 use PHPUnit\Framework\TestCase;
+use function Siler\File\concat_files;
+use function Siler\File\recur_iter_dir;
 use function Siler\File\recursively_iterated_directory;
 
 class FileTest extends TestCase
@@ -21,5 +23,13 @@ class FileTest extends TestCase
         $dir = recursively_iterated_directory("$basedir/fixtures", '/\.txt$/');
 
         $this->assertContains("$basedir/fixtures/php_input.txt", $dir);
+    }
+
+    public function testConcatFiles()
+    {
+        $basedir = dirname(__DIR__, 2);
+        $result = concat_files(recur_iter_dir("$basedir/fixtures/concat/"));
+
+        $this->assertSame("bar\nfoo", $result);
     }
 }
