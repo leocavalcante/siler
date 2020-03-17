9b2a05de3927578d086fb9a03505316c5ed24662
leocavalcante <lc@leocavalcante.com>
2020-03-16 22:06:18 -0300

    Add test to new Arr module

---
+++ tests/ArrTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Siler\Test\Unit\Prelude;
+
+use PHPUnit\Framework\TestCase;
+use function Siler\Arr\set;
+
+class ArrTest extends TestCase
+{
+    public function testSet()
+    {
+        $fixture = ['foo' => ['bar' => 'baz']];
+        $expected = ['foo' => ['bar' => 'qux']];
+        $this->assertSame($expected, set($fixture, 'foo.bar', 'baz'));
+    }
+}
