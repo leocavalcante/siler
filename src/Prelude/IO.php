fd8955ddd75dbdbde9f1d8d03c3bc0b9f587ec56
leocavalcante <lc@leocavalcante.com>
2020-06-23 11:34:10 -0300

    Initial draft for fetch API

--- src/Prelude/IO.php
+++ src/Prelude/IO.php
@@ -2,6 +2,8 @@
 
 namespace Siler\IO;
 
+use function Siler\Encoder\Json\encode;
+
 /**
  * Prints a string to the output and adds an EOL.
  *
@@ -32,4 +34,58 @@ function csv_to_array(string $filename, string $delimiter = ',', int $length = 0
     fclose($handle);
 
     return $arr;
+}
+
+function fetch(string $url, array $opts = [])
+{
+    $opts = array_merge([
+        'body' => '',
+        'follow' => true,
+        'headers' => [],
+        'method' => 'get',
+        'timeout' => 30,
+        'url' => $url,
+        'verify' => false,
+        'json' => null,
+    ], $opts);
+
+    $error = null;
+    $headers = [];
+    $ch = curl_init();
+
+    if ($opts['json'] !== null) {
+        $headers[] = 'Content-Type: application/json';
+        $opts['body'] = encode($opts['body']);
+    }
+
+    foreach ($opts['headers'] as $h_name => $h_value) {
+        $headers[] = "$h_name: $h_value";
+    }
+
+    curl_setopt_array($ch, [
+        CURLOPT_CUSTOMREQUEST => strtoupper($opts['method']),
+        CURLOPT_FOLLOWLOCATION => $opts['follow'],
+        CURLOPT_HTTPHEADER => $headers,
+        CURLOPT_POSTFIELDS => $opts['body'],
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_SSL_VERIFYPEER => $opts['verify'],
+        CURLOPT_TIMEOUT => $opts['timeout'],
+        CURLOPT_URL => $opts['url'],
+    ]);
+
+    $response = curl_exec($ch);
+    $status = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
+
+    if (empty($response)) {
+        $error = curl_error($ch);
+        $response = null;
+    }
+
+    curl_close($ch);
+
+    return [
+        'error' => $error,
+        'response' => $response,
+        'status' => $status,
+    ];
 }
