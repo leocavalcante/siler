846793e30e464cf16e4d407bc6fccdd6a9c0aed7
leocavalcante <lc@leocavalcante.com>
2020-06-25 22:02:30 -0300

    Initial tests for Fetch API

--- tests/Unit/Prelude/IOTest.php
+++ tests/Unit/Prelude/IOTest.php
@@ -4,6 +4,7 @@ namespace Siler\Test\Unit\Prelude;
 
 use PHPUnit\Framework\TestCase;
 use function Siler\IO\csv_to_array;
+use function Siler\IO\fetch;
 use function Siler\IO\println;
 
 class IOTest extends TestCase
@@ -18,5 +19,14 @@ class IOTest extends TestCase
     {
         $arr = csv_to_array(__DIR__ . '/../../fixtures/test.csv');
         $this->assertSame([['1', 'foo'], ['2', 'bar'], ['3', 'baz']], $arr);
+    }
+
+    public function testFetch()
+    {
+        $args = ['foo' => 'bar'];
+
+        ['data' => $data, 'headers' => $headers] = fetch('https://postman-echo.com/get', ['query' => $args]);
+
+        $this->assertSame($args, $data['args']);
     }
 }
