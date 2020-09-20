8d261a5cdb01e7e59ed00360de1646274987272a
leocavalcante <lc@leocavalcante.com>
2020-09-19 23:43:04 -0300

    Composer update

---
+++ tests/Unit/Config/ConfigTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Siler\Test\Unit\Config;
+
+use function Siler\Config\{load, config};
+
+class ConfigTest
+{
+    public function testLoad()
+    {
+
+    }
+}
